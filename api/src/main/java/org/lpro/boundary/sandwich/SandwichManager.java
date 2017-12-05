d8bdda276e07823ed18c54332e5549f46cda2680
Steven W <email@email.fr>
2017-12-05 16:38:36 +0100

    Get ALl

--- api/src/main/java/org/lpro/boundary/sandwich/SandwichManager.java
+++ api/src/main/java/org/lpro/boundary/sandwich/SandwichManager.java
@@ -1,6 +1,8 @@
 package org.lpro.boundary.sandwich;
 
 import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -9,5 +11,12 @@ public class SandwichManager {
 
     @PersistenceContext
     EntityManager em;
+
+    public JsonObject getMeta() {
+        return Json.createObjectBuilder()
+                .add("count", this.em.findAll())
+                .add("date", '05-12-2017')
+                .build();
+    }
 
 }
--- api/src/main/java/org/lpro/boundary/sandwich/SandwichRessource.java
+++ api/src/main/java/org/lpro/boundary/sandwich/SandwichRessource.java
@@ -4,10 +4,13 @@ import org.lpro.entity.Sandwich;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Stateless
 @Path("sandwichs")
@@ -16,6 +19,33 @@ import javax.ws.rs.core.MediaType;
 public class SandwichRessource {
 
     @Inject
-    SandwichManager cm;
+    SandwichManager sm;
+
+    @GET
+    public Response getSandwichs() {
+        JsonObject json = Json.createObjectBuilder()
+                .add("type", "collection")
+                .add("meta", this.getMeta())
+                .add("sandwichs", this.getSandwichsList())
+                .build();
+        return Response.ok(json).build();
+    }
+
+    private JsonArray getSandwichsList() {
+        JsonArrayBuilder jab = Json.createArrayBuilder();
+        this.sm.findAll().forEach((s) -> {
+            jab.add(buildJson(s));
+        });
+        return jab.build();
+    }
+
+    private JsonObject buildJson(Sandwich c) {
+        return Json.createObjectBuilder()
+                .add("id", c.getId())
+                .add("nom", c.getNom())
+                .add("description", c.getDescription())
+                .add("type_pain", c.getPain())
+                .build();
+    }
 
 }
