0c3fecfd5af64804c8b1aca7fa2c3dec4e5a8bed
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2017-12-05 16:27:11 +0100

    Adding methods in SandwichManager

--- api/src/main/java/org/lpro/boundary/sandwich/SandwichManager.java
+++ api/src/main/java/org/lpro/boundary/sandwich/SandwichManager.java
@@ -1,8 +1,13 @@
 package org.lpro.boundary.sandwich;
 
+import org.lpro.entity.Sandwich;
+
 import javax.ejb.Stateless;
+import javax.persistence.CacheStoreMode;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
 
 @Stateless
 public class SandwichManager {
@@ -10,6 +15,13 @@ public class SandwichManager {
     @PersistenceContext
     EntityManager em;
 
+    public Sandwich findById(long id) {
+        return this.em.find(Sandwich.class, id);
+    }
 
-
+    public List<Sandwich> findAll() {
+        Query q = this.em.createNamedQuery("Sandwich.findAll", Sandwich.class);
+        q.setHint("javax.persistance.cache.storeMode", CacheStoreMode.REFRESH);
+        return q.getResultList();
+    }
 }
--- api/src/main/java/org/lpro/boundary/sandwich/SandwichRessource.java
+++ api/src/main/java/org/lpro/boundary/sandwich/SandwichRessource.java
@@ -18,4 +18,5 @@ public class SandwichRessource {
     @Inject
     SandwichManager cm;
 
+
 }
