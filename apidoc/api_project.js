ee1a8cc9b7f52ae0103470601252e831df4f097c
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2017-12-29 01:22:45 +0100

    Correction bugs

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-12-29T00:19:15.914Z",
+    "time": "2017-12-29T00:22:22.382Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
